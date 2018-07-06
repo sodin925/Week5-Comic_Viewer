d660e49436400b002e4191ef42020f6a656031a7
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-07-07 00:46:54 +0800

    20%

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// Sass configuration
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./*.scss')//抓
+    .pipe(sass())
+    .pipe(gulp.dest('./CSS/'))//建立
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./css/*.scss', ['sass']);
+})
